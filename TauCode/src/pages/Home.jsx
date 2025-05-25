86795bd9c937fe781ca4ab9d3abbee169a83a632
Dhruv Suthar <dhruvsutar779@gmail.com>
2025-05-25 12:14:56 +0530

    apperance changes

--- TauCode/src/pages/Home.jsx
+++ TauCode/src/pages/Home.jsx
@@ -33,20 +33,44 @@ const Home = () => {
         </ul>
       </nav>
 
-      <section className='Hero'>
-        <img src="../70881520585.png" alt="" />
-      </section>
-      
-      <section className='about'>
-        <div className="content">
-          <h1 className='text-4xl font-bold text-center'>Welcome to TauCode</h1>
-          <p className='text-lg text-center mt-4'>
-            TauCode is a powerful tool designed to help you write, test, and debug your code with ease. 
-            Whether you're a beginner or an experienced developer, TauCode provides a user-friendly interface 
-            and a rich set of features to enhance your coding experience.
-          </p>
+      <section className='Hero flex'>
+
+        <div className="leftdiv">
+        <div className="content flex flex-col justify-center gap-4">
+          <div className="spandiv">
+            <span className='text-5xl'> Ram Ram </span><span className='text-5xl'> Mitar!!!!! 🙏</span>
+            <p className="text-left text-lg mt-6 text-gray-700">
+              TauCode mein semicolon lagana optional hai, lekin code paar break lagana na se😌☕<br/>
+              "Badmosi aab coding mein bhi chalegi, par yaar ke saath chalegi!"
+            </p>
+            <br />
+            
+            <p className="text-start text-lg font-semibold mt-6 text-gray-800">
+              Tau kehwe: "Error na hai, thodi si hichki se… seedha kar denge!" 🔧🔥<br/>
+              TauCode likhne waale line-by-line nahi, <i>dialogue-by-dialogue</i> likhte hain!
+            </p>
+            <br/>
+
+          </div>
         </div>
+          <div className='Buttonsdiv flex  gap-2 mt-8 w-full'>
+          <Link to="/compiler" className='btn btn-primary rounded-2xl text-1xl text-white bg-black '>PlayGround</Link>
+          <Link to="/documentation" className='btn btn-secondary rounded-2xl text-1xl'>Documentation</Link>
+        </div>
+        </div>
+        <div className="imagediv">
+          <img src="../70881520585.png" alt="" />
+        </div>
+        
       </section>
+
+      <section className='Slogan flex justify-center items-center mt-10'>
+        <h1 className='text-4xl text-white '>"Thane reel mai coder dekhe se, mka real mai coder ham se 😎"</h1>
+      </section>
+
+
+      
+      
 
     </>
   )
