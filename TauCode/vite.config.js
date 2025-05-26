8ac7c50c980d111c964f768828bad0643b57d19b
Dhruv Suthar <dhruvsutar779@gmail.com>
2025-05-26 18:41:15 +0530

    issues with deployment

--- TauCode/vite.config.js
+++ TauCode/vite.config.js
@@ -1,22 +1,8 @@
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    host: 'localhost',
-    port: 5173,
-    strictPort: true,
-  },
-  build: {
-    assetsInlineLimit: 4096, // 4kb
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
-});
+  plugins: [react(), tailwindcss()],
+})
